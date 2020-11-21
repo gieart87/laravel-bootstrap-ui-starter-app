85194b7186b84d4a8fc040d2bcb40bb0046001ca
sugiarto <sugiarto@gie-art.com>
2020-11-22 00:05:10 +0700

    added: dashboard ui using stisla bootstrap 4 theme

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -10,4 +10,9 @@ use Illuminate\Routing\Controller as BaseController;
 class Controller extends BaseController
 {
     use AuthorizesRequests, DispatchesJobs, ValidatesRequests;
+
+    public function __construct()
+    {
+        
+    }
 }
